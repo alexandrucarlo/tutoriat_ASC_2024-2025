2a8b0d42cbb0ffd7ba1f9f5f63902ec11333c981
alext <184996106+alexandrucarlo@users.noreply.github.com>
2025-11-08 00:40:11 +0200

    Adaugare exercitiu 3 tutoriat 4

---
+++ exercitii_tut4/suma_cub.c
@@ -0,0 +1,11 @@
+int main() {
+	int vec[10] = {5, 6, 76, 543, 43, 3, 8, 65, 67, 420};
+	int vec_size = 10;
+	int sum = 0;
+	
+	for (int i = 0; i < vec_size; i++) {
+		sum += vec[i] * vec[i] * vec[i];
+	}
+
+	return 0;
+}
---
+++ exercitii_tut4/suma_cub.s
@@ -0,0 +1,48 @@
+.data # in sectiunea .data ne declaram variabilele
+vec: .long 5, 6, 76, 543, 43, 3, 8, 65, 67, 420
+vec_size: .long 10
+
+.text # iar in sectiune .text ne declaram codul/instructiunile
+.global main
+
+main:
+
+lea vec, %esi # invatam adresa vectorului si o mutam in registrul esi 
+movl $0, %ebx # vom folosi registrul ebx ca variabila in care stocam suma
+
+
+movl $0, %ecx # iar registrul ecx ca fiind i-ul (din for-ul in C)
+sum_loop: # iar acum ne construim bucla, ca sa ne adunam fiecare numar
+
+# instructiunea cmp este neintuitiva si o ia dreapta-stanga in loc de stanga-dreapta
+cmpl %ecx, vec_size 
+jle sum_loop_end # ...daca i >= vec_size (care e complementara de la i < vec_size), iesim din bucla 
+
+
+# aici folosim 4 pentru ca un .long (int) are 4 bytes (32 biti)
+movl (%esi, %ecx, 4), %eax # incarcam valoarea de la vec[i] in eax
+# movl vec(, %ecx, 4), %eax # merge si asa!! (si nici nu mai trebuie sa folositi instructiunea lea)
+
+# inmultim de doua ori eax cu el insusi (ATENTIE - INCORECT!!!)
+# imull %eax
+# imull %eax # daca pur si simplu inmultim de doua ori cu el, nu obtinem n^3, ci n^4 (ex 5*5=25, 25*25=625=5^4)
+
+# copiem valoarea originala a lui eax si facem inmultirea folosind copia
+movl %eax, %edi # salvam valoarea originala a lui eax in alt registru (oricare in afara de cele folosite deja, si edx (pentru ca va fi modifcat de imul))
+imull %edi
+imull %edi # inmultim de doua ori eax cu edi - va da raspunsul corect
+
+
+addl %eax, %ebx # si apoi adunam eax la registrul ebx (pe care l-am stabilit ca variabila suma)
+
+incl %ecx # i++ (sau addl $1, %ecx)
+jmp sum_loop # ne intoarcem la capul buclei
+
+sum_loop_end:
+# aici continuam cu alte chestii din program (facem chestii cu suma obtinuta, sau poate vrem sa calculam alte chestii)
+# dar noi o sa inchidem programul :P
+
+exit: # return 0
+movl $1, %eax # syscall-ul 1 (exit), asta face si limbajul C in interiorul sau
+movl $0, %ebx # codul pe care il va returna programul, adica 0
+int $0x80 # apeleaza syscall-ul! (si inchidem programul corect)
